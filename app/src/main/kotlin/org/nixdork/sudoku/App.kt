210f3e500a13a66207d70a481e9d1508b7a1e73c
Jonas Gorauskas <jgorauskas@gmail.com>
2022-02-21 07:51:42 -0800

    cleanup + fix fromFile

--- app/src/main/kotlin/org/nixdork/sudoku/App.kt
+++ app/src/main/kotlin/org/nixdork/sudoku/App.kt
@@ -1,21 +1,3 @@
-/* Solve Every Sudoku Puzzle
- * 
- *  Throughout this program we have:
- *    r is a row,    e.g. "A"
- *    c is a column, e.g. "3"
- *    s is a square, e.g. "A3"
- *    d is a digit,  e.g. "9"
- *    u is a unit,   e.g. ["A1","B1","C1","D1","E1","F1","G1","H1","I1"]
- *    grid is a grid,e.g. 81 non-blank chars, e.g. starting with ".18...7...
- *    values is a map of possible values, e.g. {"A1":"12349", "A2":"8", ...}
- * 
- * References used:
- *   http://www.scanraid.com/BasicStrategies.htm
- *   http://www.sudokudragon.com/sudokustrategy.htm
- *   http://www.krazydad.com/blog/2005/09/29/an-index-of-sudoku-strategies/
- *   http://www2.warwick.ac.uk/fac/sci/moac/currentstudents/peter_cock/python/sudoku/   
- */
-
 package org.nixdork.sudoku
 
 import java.io.File
@@ -24,9 +6,11 @@ val digits = "123456789"
 val rows = "ABCDEFGHI"
 val cols = digits
 val squares = cross(rows, cols)
-val unitlist = cols.map { c -> cross(rows, "$c") } +
+val unitlist = cols.map { c -> 
+    cross(rows, "$c") } +
     rows.map { r -> cross("$r", cols) } +
-    listOf("ABC","DEF","GHI").flatMap { rs -> listOf("123","456","789").map { cs -> cross(rs, cs) } }
+    listOf("ABC","DEF","GHI").flatMap { rs -> listOf("123","456","789").map { cs -> cross(rs, cs) } 
+}
 val units = squares.map { s -> s to unitlist.filter { u -> u.contains(s) } }.toMap()
 val peers = squares.map { s -> s to units[s]!!.flatten().toSortedSet() - listOf(s).toSet() }.toMap()
 
@@ -84,19 +68,17 @@ fun gridValues(grid: String) =
 //////////////////////////////// Utilities ////////////////////////////////
 
 // Parse a file into a list of strings, separated by `sep`
-fun fromFile(fileName: String, sep: String?): List<String> {
+fun fromFile(fileName: String, sep: String? = null): List<String> {
     return if (sep == null) {
         File(fileName).readLines()
     } else {
         File(fileName).readText(Charsets.UTF_8).split(sep)
-    }.filter { it.length == 0 }
+    }.filter { it.length != 0 }
 }
 
 //////////////////////////////// System test ////////////////////////////////
 
 fun main() {
     test()
-    gridValues(grid1)
-    gridValues(grid2)
     // println(squares)
 }
